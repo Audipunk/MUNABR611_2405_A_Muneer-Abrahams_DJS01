c809ab76a28e7ea41255046c2209da6728f3b3e4
Muneer Abrahams <audipunkwp@gmail.com>
2025-01-19 20:00:53 +0000

    update js

--- index.js
+++ index.js
@@ -6,40 +6,28 @@
  */
 
 // Given Parameters
-const velocityKmH = 10000; // velocity (km/h)
-const accelerationMs2 = 3; // acceleration (m/s^2)
-const timeInSeconds = 3600; // seconds (1 hour)
-const initialDistanceKm = 0; // distance (km)
-const initialFuelKg = 5000; // remaining fuel (kg)
-const fuelBurnRateKgs = 0.5; // fuel burn rate (kg/s)
-
-// Convert acceleration from m/s^2 to km/h^2
-const accelerationKmH2 = accelerationMs2 * 12960; // 1 m/s^2 = 12960 km/h^2
+const initialVelocityKMH = 10000; // velocity (km/h)
+const accelerationKMH = 3 * 12960; // acceleration (coverted from m/s^2 to km/h^2)
+const timeHours = 3600 / 3600 // seconds (1 hour)
+const initialDistanceKM = 0; // distance (km)
+const fuelKG = 5000; // remaining fuel (kg)
+const fuelBurnRateKG = 0.5 * 3600; // fuel burn rate converted from (kg/s) to (kg/h)
 
 
-const calcNewDistance = initialDistanceKm + (velocityKmH*(timeInSeconds/3600)); //calcultes new distance
-const calcRemainingFuelKg = initialFuelKg - (fuelBurnRateKgs * timeInSeconds); //calculates remaining fuel
-const currentVelocityKmH = velocityKmH + (accelerationKmH2 * (timeInSeconds / 3600)) //calculates new velocity based on acceleration
 
-// Calculate new velocity based on acceleration and time
-function calcNewVelocityKmH(velocityKmH, accelerationMs2, timeInSeconds){
-  // check if the parameters are not negative
-  if(velocityKmH < 0 || accelerationMs2 < 0 || timeInSeconds < 0){
-    throw new Error('Parameters cannot be negative')
+const newDistance = initialDistanceKM + (initialVelocityKMH * timeHours) //calcultes new distance
+const remainingFuel = fuelKG - (fuelBurnRateKG * timeHours) //calculates remaining fuel
+
+// Pick up an error with how the function below is called and make it robust to such errors
+const calcNewVelocity = (initialVelocityKMH, accelerationKMH, timeHours) => { 
+  if (initialVelocityKMH < 0 || accelerationKMH < 0 || timeHours < 0) {
+    throw new Error('Invalid entry: Entry fields can not be negative values')
   }
-
-    // convert acceleration from m/s^2 to km/h^2
-  const accelerationKmH2 = accelerationMs2 * 12960; // 1 m/s^2 = 12960 km/h^2
-  const timeInHours = timeInSeconds / 3600
-  return velocityKmH + (accelerationKmH2 * timeInHours)
+  return initialVelocityKMH + (accelerationKMH * timeHours)
 }
 
-try {
-  const currentVelocityKmH = calcNewVelocityKmH(velocityKmH, accelerationMs2, timeInSeconds)
-  console.log(`Corrected New Velocity: ${currentVelocityKmH} km/h`);
-  console.log(`Corrected New Distance: ${calcNewDistance} km`);
-  console.log(`Corrected Remaining Fuel: ${calcRemainingFuelKg} kg`);
-} catch (error) {
-  console.log(error.message)
-}
+const newVelocity = calcNewVelocity(initialVelocityKMH, accelerationKMH, timeHours) //calculates new velocity based on acceleration
 
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
